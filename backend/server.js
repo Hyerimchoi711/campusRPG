7967903888229f3f0d79be8ee869f5873dddb27c
hyerim <heon598555@gmail.com>
2026-03-21 21:08:23 +0900

    개발 환경 구축&보일러플레이트 코드 작성

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Basic Route
+app.get('/', (req, res) => {
+  res.send('Campus Life RPG API is running!');
+});
+
+// TODO: 라우터 연결 (예: app.use('/api/users', userRoutes))
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
